91141855cbab6d5bbce0af42c5340485c6ce0c78
Alexar <github@alexar.me>
2023-06-01 01:49:04 +0930

    Produces a request for proposal.

---
+++ PROPOSAL.md
@@ -0,0 +1,51 @@
+---
+
+# Cleanux
+
+Cleanux is a command-line utility for cleaning up and optimizing Ubuntu systems. It provides a set of cleanup steps to remove unnecessary packages, clear cache, remove old kernels, clean up APT cache, remove orphaned packages, clean up old configuration files, and remove unused packages and their associated configuration files. Cleanux helps improve disk space utilization and system performance by removing non-useful system files.
+
+## Usage
+
+To run Cleanux and perform the system cleanup, follow these steps:
+
+1. Open a terminal on your Ubuntu system.
+
+2. Navigate to the directory where you saved the Cleanux script.
+
+3. Run the script to execute Cleanux.
+
+4. Follow the on-screen prompts to confirm or skip each cleanup step.
+
+5. Cleanux will display the amount of disk space saved after each successful cleanup step.
+
+6. Once the cleanup process is complete, Cleanux will display a message indicating the completion of the cleanup.
+
+## Cleanup Steps
+
+Cleanux performs the following cleanup steps to optimize your Ubuntu system:
+
+1. Remove Unnecessary Packages: This step removes unnecessary packages that are no longer required by any installed software.
+
+2. Clear Package Cache: This step clears the cache of downloaded packages stored in the APT package cache.
+
+3. Remove Old Kernels: This step removes old kernel packages that are no longer in use.
+
+4. Clean Up APT Cache: This step cleans up the APT cache by removing downloaded package files that are no longer needed.
+
+5. Remove Orphaned Packages: This step identifies and removes orphaned packages that are no longer required by any installed software.
+
+6. Clean Up Old Configuration Files: This step removes old configuration files associated with packages that have been removed but not purged.
+
+7. Remove Unused Packages and Configuration Files: This step removes unused packages and their associated configuration files.
+
+## Contributing
+
+Cleanux is an open-source project, and contributions are welcome. If you encounter any issues, have suggestions for improvements, or would like to contribute to the project, please visit the Cleanux GitHub repository.
+
+## License
+
+Cleanux is released under the MIT License.
+
+---
+
+Feel free to modify the document according to your specific project details and requirements.
--- cleanux.sh
+++ cleanux.sh
@@ -24,35 +24,56 @@ calculate_disk_space() {
   eval "$2"
   local after=$(df -h --output=avail "$1" | sed -n 2p | awk '{print $1}')
   local saved=$(echo "$before - $after" | bc)
-  local saved_formatted=$(numfmt --to=iec-i --suffix=B "$saved")
+  local saved_formatted=$(format_size "$saved")
   echo "$(tput setaf 2)Saved space: $saved_formatted$(tput sgr0)"
   echo
 }
 
-# Function to display confirmation prompt and check if there are packages to purge
-confirm_with_purge_check() {
-  local command=$1
-  local purge_check_command=$2
-
-  if ! eval "$purge_check_command"; then
-    echo "$(tput setaf 1)No packages to purge.$(tput sgr0)"
-    echo
-    return 1
+# Function to format the size with appropriate units
+format_size() {
+  local size=$1
+  local unit=""
+  if (( size < 1024 )); then
+    echo "$size B"
+  elif (( size < 1048576 )); then
+    size=$(printf "%.2f" "$(bc -l <<< "$size / 1024")")
+    echo "$size KB"
+  elif (( size < 1073741824 )); then
+    size=$(printf "%.2f" "$(bc -l <<< "$size / 1048576")")
+    echo "$size MB"
+  else
+    size=$(printf "%.2f" "$(bc -l <<< "$size / 1073741824")")
+    echo "$size GB"
   fi
-
-  if confirm "$command"; then
-    return 0
-  fi
-
-  return 1
 }
+
+# Check if deborphan is installed
+if ! command -v deborphan &> /dev/null; then
+  echo "$(tput setaf 1)Deborphan is not installed.$(tput sgr0)"
+  echo "$(tput setaf 3)You can install deborphan by running the following command:$(tput sgr0)"
+  echo "$(tput setaf 6)sudo apt-get install deborphan$(tput sgr0)"
+  echo
+  if [[ $auto_accept != "a" ]]; then
+    read -p "$(tput setaf 6)Do you want to install deborphan? (y/n/a): $(tput sgr0)" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      sudo apt-get install deborphan
+    elif [[ $REPLY =~ ^[Aa]$ ]]; then
+      auto_accept="a"
+    fi
+  fi
+fi
 
 # Function to remove unnecessary packages
 remove_unnecessary_packages() {
   local command="sudo apt autoremove"
-  local purge_check_command="dpkg -l | awk '/^rc/ { print \$2 }' | grep -q '.'"
-  if confirm_with_purge_check "$command" "$purge_check_command"; then
-    calculate_disk_space "/" "$command"
+  if confirm "$command"; then
+    if dpkg -l | awk '/^rc/ { print $2 }' | grep -q "."; then
+      calculate_disk_space "/" "$command"
+    else
+      echo "$(tput setaf 1)No packages to purge.$(tput sgr0)"
+      echo
+    fi
   fi
 }
 
@@ -67,9 +88,13 @@ clear_package_cache() {
 # Function to remove old kernels
 remove_old_kernels() {
   local command="sudo apt autoremove --purge"
-  local purge_check_command="dpkg -l | awk '/^rc/ { print \$2 }' | grep -q '.'"
-  if confirm_with_purge_check "$command" "$purge_check_command"; then
-    calculate_disk_space "/" "$command"
+  if confirm "$command"; then
+    if dpkg -l | awk '/^rc/ { print $2 }' | grep -q "."; then
+      calculate_disk_space "/" "$command"
+    else
+      echo "$(tput setaf 1)No packages to purge.$(tput sgr0)"
+      echo
+    fi
   fi
 }
 
@@ -83,27 +108,15 @@ clean_up_apt_cache() {
 
 # Function to remove orphaned packages
 remove_orphaned_packages() {
-  if ! command -v deborphan &> /dev/null; then
-    echo "$(tput setaf 1)Deborphan is not installed.$(tput sgr0)"
-    echo "$(tput setaf 3)You can install deborphan by running the following command:$(tput sgr0)"
-    echo "$(tput setaf 6)sudo apt-get install deborphan$(tput sgr0)"
-    echo
-    if [[ $auto_accept != "a" ]]; then
-      read -p "$(tput setaf 6)Do you want to install deborphan? (y/n/a): $(tput sgr0)" -n 1 -r
-      echo
-      if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo apt-get install deborphan
-      elif [[ $REPLY =~ ^[Aa]$ ]]; then
-        auto_accept="a"
-      fi
-    fi
-  fi
-
   if command -v deborphan &> /dev/null; then
     local command="deborphan | xargs sudo apt-get -y remove --purge"
-    local purge_check_command="dpkg -l | awk '/^rc/ { print \$2 }' | grep -q '.'"
-    if confirm_with_purge_check "$command" "$purge_check_command"; then
-      calculate_disk_space "/" "$command"
+    if confirm "$command"; then
+      if dpkg -l | awk '/^rc/ { print $2 }' | grep -q "."; then
+        calculate_disk_space "/" "$command"
+      else
+        echo "$(tput setaf 1)No packages to purge.$(tput sgr0)"
+        echo
+      fi
     fi
   fi
 }
@@ -111,17 +124,20 @@ remove_orphaned_packages() {
 # Function to clean up old configuration files
 clean_up_old_config_files() {
   local command="sudo dpkg --purge \$(dpkg -l | awk '/^rc/ { print \$2 }')"
-  local purge_check_command="dpkg -l | awk '/^rc/ { print \$2 }' | grep -q '.'"
-  if confirm_with_purge_check "$command" "$purge_check_command"; then
-    calculate_disk_space "/" "$command"
+  if confirm "$command"; then
+    if dpkg -l | awk '/^rc/ { print $2 }' | grep -q "."; then
+      calculate_disk_space "/" "$command"
+    else
+      echo "$(tput setaf 1)No packages to purge.$(tput sgr0)"
+      echo
+    fi
   fi
 }
 
 # Function to remove unused packages and their associated configuration files
 remove_unused_packages() {
   local command="sudo apt-get autoremove --purge"
-  local purge_check_command="dpkg -l | awk '/^rc/ { print \$2 }' | grep -q '.'"
-  if confirm_with_purge_check "$command" "$purge_check_command"; then
+  if confirm "$command"; then
     calculate_disk_space "/" "$command"
   fi
 }
